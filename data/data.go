af2db731e5ee23f59921c09cc8c29f8e4ca3ed40
Alexey Andreev <bezumny.tochk@gmail.com>
2018-06-29 14:13:49 +0400

    Add get data api method

--- data/data.go
+++ data/data.go
@@ -1,1 +1,34 @@
 package data
+
+import (
+	"github.com/tochk/cockroachdb-admin-backend/connections_manager"
+)
+
+type Query struct {
+	Token string `json:"token"`
+	Db    string `json:"db"`
+	Table string `json:"table"`
+}
+
+type Answer map[string]interface{}
+
+func GetData(query Query) (answer []Answer, err error) {
+	conn, err := connections_manager.GetConnection(query.Token)
+	if err != nil {
+		return nil, err
+	}
+	_, err = conn.Exec("USE " + query.Db)
+	if err != nil {
+		return nil, err
+	}
+	rows, err := conn.Queryx("SELECT * FROM " + query.Table)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		results := make(map[string]interface{})
+		err = rows.MapScan(results)
+		answer = append(answer, results)
+	}
+	return
+}
