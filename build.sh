fd876955a42e34e3b19ef91b9b265cb1452e4e9a
Alexey Andreev <bezumny.tochk@gmail.com>
2018-09-07 10:05:42 +0400

    Add create table

--- build.sh
+++ build.sh
@@ -5,6 +5,4 @@ set -u
 
 mkdir -p build
 
-env GOOS=linux GOARCH=amd64 go build -o build/cdbadmin-linux-amd64
-env GOOS=windows GOARCH=amd64 go build -o build/cdbadmin-windows-amd64.exe
-env GOOS=darwin GOARCH=amd64 go build -o build/cdbadmin-darwin-amd64
+gox -arch="386 amd64" -os="darwin windows linux" -output="./build/crdbadmin_{{.OS}}_{{.Arch}}"
--- tables/create.go
+++ tables/create.go
@@ -1,14 +1,33 @@
 package tables
 
-import "github.com/tochk/cockroachdb-admin-backend/connections_manager"
+import (
+	"strings"
+
+	"github.com/tochk/cockroachdb-admin-backend/connections_manager"
+)
 
 type CreateQuery struct {
 	Token    string `json:"token"`
 	Database string `json:"database"`
 	Table    string `json:"table"`
+	Fields   []struct {
+		Name    string `json:"name"`
+		Type    string `json:"type"`
+		Length  string `json:"length"`
+		Key     string `json:"key"`
+		Default string `json:"default"`
+		Null    bool   `json:"null"`
+	} `json:"fields"`
+	Constraints []struct {
+		Name  string `json:"name"`
+		Check string `json:"check"`
+	} `json:"constraints"`
+	Indexes []struct {
+		Name   string `json:"name"`
+		Fields string `json:"fields"`
+	} `json:"indexes"`
 }
 
-//todo add normal create table
 func CreateTable(query CreateQuery) (tables []Tables, err error) {
 	conn, err := connections_manager.GetConnection(query.Token)
 	if err != nil {
@@ -18,7 +37,34 @@ func CreateTable(query CreateQuery) (tables []Tables, err error) {
 	if err != nil {
 		return tables, err
 	}
-	_, err = conn.Exec("CREATE TABLE " + query.Table)
+
+	q := "CREATE TABLE " + query.Table + "(\n"
+	for _, e := range query.Fields {
+		q += e.Name + " " + e.Type + " "
+		if e.Length != "" {
+			q += "(" + e.Length + ") "
+		}
+		if e.Key != "" {
+			q += e.Key + " "
+		}
+		if !e.Null {
+			q += "NOT NULL "
+		}
+		if e.Default != "" {
+			q += "DEFAULT " + e.Default
+		}
+		q += ",\n"
+	}
+	for _, e := range query.Constraints {
+		q += "CONSTRAINT " + e.Name + " CHECK " + e.Check + ",\n"
+	}
+	for _, e := range query.Indexes {
+		q += "INDEX " + e.Name + " (" + e.Fields + "),\n"
+	}
+	q = strings.Trim(q, ",\n")
+	q += "\n);"
+
+	_, err = conn.Exec(q)
 	if err != nil {
 		return tables, err
 	}
